87c6709694ba34fb62788900c69673e53ca8ada3
pl17kv23 <73066353+pl17kv23@users.noreply.github.com>
2022-10-24 14:48:03 +0300

    Add files via upload

---
+++ HomeWorks/Seminar1/Program.cs
@@ -0,0 +1,70 @@
+﻿//                                             Задание 1
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+/* int a, b;
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine()); 
+b = Convert.ToInt32(Console.ReadLine());
+  if (a > b)
+ 
+     { 
+        Console.WriteLine($" max = {a}");
+     }
+     else
+     {
+        Console.WriteLine($"max = {b}");
+      }
+     Console.ReadLine();
+*/
+     //                                          Задание 2
+
+
+/*
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*
+int a, b, c;
+Console.WriteLine("Введите три числа : ");
+a = Convert.ToInt32(Console.ReadLine()); 
+b = Convert.ToInt32(Console.ReadLine());
+c =Convert.ToInt32(Console.ReadLine());
+if (a > b)
+   if (a > c)
+      { 
+        Console.WriteLine($" max = {a}");
+     }
+     else
+     {
+        Console.WriteLine($"max = {c}");
+     }
+     
+else if (b > c)
+{
+ Console.WriteLine($"max = {b}");
+}
+else
+{
+ Console.WriteLine($"max = {c}");
+}
+*/
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write ("Введите число : ");
+int num = Convert.ToInt32 (Console.ReadLine ());
+int result = num % 2;
+if (result == 0)
+{
+    Console.WriteLine ("Это четное число!");
+}
+else
+{
+    Console.WriteLine ("Это нечетное число!");
+}
---
+++ HomeWorks/seminar2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+/*
+int CupNumber (int num) 
+{
+    int result = num / 10 % 10; 
+    return result;     
+}
+        int RandomNumber = new Random().Next(456, 919);
+        int result = CupNumber(RandomNumber);
+        Console.WriteLine($" Искомое число {result}");
+*/
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+/*
+int CupNumber (int num) 
+{
+    if (num == RandomNumber)     <<<-------------------тут у меня затык
+    {
+    int result = num % 10; 
+    return result;
+    }
+    else
+    {
+        Console.WriteLine("-> Третьей цифры нет");
+    }  
+}
+        int RandomNumber = new Random().Next(456, 457);
+        int result = CupNumber(RandomNumber);
+        Console.WriteLine($" Искомое число {result}");
+*/
+
+
+
+/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+bool TrueOfFalse(int num)
+{
+    if (num == 6 || num == 7)
+    return true;
+    else return false;
+}
+
+
+Console.WriteLine("num ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool IsDivisionable = TrueOfFalse(num);
+Console.WriteLine(IsDivisionable);
